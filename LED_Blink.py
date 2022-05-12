e1e13078edb901b365caf507c27f43f0355961dd
dwjeong Raspberry Pi <zlzl2778@gmail.com>
2022-05-12 20:22:31 +0900

    GPIO Example 2

--- LED_Blink.py
+++ LED_Blink.py
@@ -1,13 +1,11 @@
-#-*- coding: utf-9 -*-
-
 import RPi.GPIO as GPIO
 import time
 led_pin = 4 #GPIO 4
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(led_pin, GPIO.out)
-for i in range(10):
+GPIO.setup(led_pin, GPIO.OUT)
+for i in range(100):
 	GPIO.output(led_pin,1)
-	time.sleep(1)
+	time.sleep(0.03)
 	GPIO.output(led_pin,0)
-	time.sleep(1)
+	time.sleep(0.03)
 GPIO.cleanup()
