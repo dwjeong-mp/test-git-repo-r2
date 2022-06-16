30586ad81766c6bab21155f5db21f80b5b077a0a
dwpi <zlzl2778@gmail.com>
2022-06-16 16:41:55 +0900

    ddee

--- final_term_homework.py
+++ final_term_homework.py
@@ -1,26 +1,28 @@
 import paho.mqtt.client as mqtt
-import RPi.GPIO as GPIO
+import time
+import RPi.GPIO as GPIO 
 
-def on_connect(client, userdata, flag, rc):
-	print("Connected with rc:" + str(rc))
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT)
+
+mqttc = mqtt.Client()
+mqttc.connect("192.168.111.95", 1883, 60)
+
+def connected(client, userdata, flag, rc): 
+	print("Connected in successfully!!")
 	client.subscribe("dw")
 
-def on_message(client, userdata, msg):
-	if 2 in msg.payload:
-		print("ON!!!")
-		GPIO.output(11, True)
+def message_revise(client, userdata, msg): 
+	if 1 in msg.payload: 
+		print("Correct signal published from phone!")
+		GPIO.output(8, True)
 	else :
-		print("OFF!!")
-		GPIO.output(11, False)
-
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.connect("localhost", 1883, 60)
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+		print("Incorrect sinal was published on phone!")
+		GPIO.output(8, False)
 
 
-client.loop_forever()
+mqttc.on_connect = connected
+mqttc.on_message = message_revise
+
+mqttc.loop_forever() 
